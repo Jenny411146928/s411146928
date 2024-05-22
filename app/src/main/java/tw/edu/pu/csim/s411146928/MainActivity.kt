1cad25d428ae78763dc86b78b0786d6215c9d106
s411146928 <jenny1211jenny@gmail.com>
2024-05-22 16:30:45 +0800

    v3_李盈蓁

--- app/src/main/java/tw/edu/pu/csim/s411146928/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/s411146928/MainActivity.kt
@@ -1,14 +1,21 @@
 package tw.edu.pu.csim.s411146928
 
+import android.R
+import android.R.id
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material3.Button
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -19,12 +26,16 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
@@ -42,7 +53,10 @@ class MainActivity : ComponentActivity() {
         setContent {
             S411146928Theme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
                     //Greeting("Android")
                     Main()
                     //FirstScreen()
@@ -51,90 +65,116 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
-}
-
-@Composable
-fun FirstScreen(navController: NavController) {
-    val context = LocalContext.current
-
-    Column(modifier = Modifier)
-    {
-
-        Text("簡介", style = TextStyle(color = Color.Blue)) //顏色改藍色
-    }
-}
 
 
-@Composable
-fun SecondScreen(navController: NavController) {
-    val context = LocalContext.current
+    @Composable
+    fun FirstScreen(navController: NavController) {
+        val context = LocalContext.current
+        var showTitle by remember { mutableStateOf(true) }
+        var expanded by remember { mutableStateOf(true) }
+        var currentImage by remember { mutableStateOf(tw.edu.pu.csim.s411146928.R.drawable.service)}
 
-    Column(modifier = Modifier)
-    {
+        Column(modifier = Modifier)
+        {
+            Text(text = if (currentImage == tw.edu.pu.csim.s411146928.R.drawable.service)"瑪利亞基金會服務總覽" else "關於App作者", style = TextStyle(color = Color.Blue))
 
-        Text("主要機構", style = TextStyle(color = Color.Red)) //顏色改紅色
-
-    }
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun Main() {
-    val navController = rememberNavController()
-    val context = LocalContext.current
-    var showMenu by remember {
-        mutableStateOf(false)
-    }
-
-    Column {
-        TopAppBar(
-            title = { Text(text = "")
-                Image(
-                    painter = painterResource(
-                        id = R.drawable.maria),
-                    contentDescription = null,
-                    modifier = Modifier
-                        .height(50.dp),
-                )
-            },
-            actions = {
-                IconButton(
-                    onClick = { showMenu = true }
-                ) {
-                    Icon(Icons.Default.MoreVert, contentDescription = "More")
-                }
-
-                DropdownMenu(
-                    expanded = showMenu, onDismissRequest = { showMenu = false }
-                ) {
-                    DropdownMenuItem(
-                        text = { Text("簡介") },
-                        onClick = { navController.navigate("JumpFirst") }
-                    )
-
-                    DropdownMenuItem(
-                        text = { Text("主要機構") },
-                        onClick = { navController.navigate("JumpSecond") }
-                    )
-
+            var alpha by remember {
+                mutableStateOf(1f)
+            }
+            LaunchedEffect(currentImage){
+                val animatable = androidx.compose.animation.core.Animatable(0f)
+                animatable.animateTo(1f, animationSpec = tween(5000)){
+                    alpha = value
                 }
             }
-        )
 
-        NavHost(navController = navController, startDestination = "JumpFirst") {
-            composable("JumpFirst") {
-                FirstScreen(navController = navController)
+            Image(painter = painterResource(currentImage), contentDescription = "service", modifier = Modifier
+                .clip(RectangleShape)
+                .fillMaxWidth()
+                .height(if(expanded)600.dp else 400.dp)
+                .clickable { expanded = !expanded }
+                .alpha(alpha)
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Button(onClick = {
+                showTitle = !showTitle
+                currentImage = if (currentImage == tw.edu.pu.csim.s411146928.R.drawable.service) {
+                    tw.edu.pu.csim.s411146928.R.drawable.j
+                } else {
+                    tw.edu.pu.csim.s411146928.R.drawable.service
+                }
+            }) {
+                Text(text = if (currentImage == tw.edu.pu.csim.s411146928.R.drawable.service) "作者：資管系李盈蓁" else "服務總覽")
             }
-            composable("JumpSecond") {
-                SecondScreen(navController = navController)
+        }
+    }
+
+
+    @Composable
+    fun SecondScreen(navController: NavController) {
+        val context = LocalContext.current
+
+        Column(modifier = Modifier)
+
+        {
+            Text("主要機構", style = TextStyle(color = Color.Red)) //顏色改紅色
+        }
+
+    }
+
+
+
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Composable
+    fun Main() {
+        val navController = rememberNavController()
+        val context = LocalContext.current
+        var showMenu by remember {
+            mutableStateOf(false)
+        }
+
+        Column {
+            TopAppBar(
+                title = { Text(text = "")
+                    Image(
+                        painter = painterResource(id = tw.edu.pu.csim.s411146928.R.drawable.maria), contentDescription = null, modifier = Modifier
+                            .height(50.dp),
+                    )
+
+                },
+                actions = {
+                    IconButton(
+                        onClick = { showMenu = true }
+                    ) {
+                        Icon(Icons.Default.MoreVert, contentDescription = "More")
+                    }
+
+                    DropdownMenu(
+                        expanded = showMenu, onDismissRequest = { showMenu = false }
+                    ) {
+                        DropdownMenuItem(
+                            text = { Text("簡介") },
+                            onClick = { navController.navigate("JumpFirst") }
+                        )
+
+                        DropdownMenuItem(
+                            text = { Text("主要機構") },
+                            onClick = { navController.navigate("JumpSecond") }
+                        )
+
+                    }
+                }
+            )
+
+            NavHost(navController = navController, startDestination = "JumpFirst") {
+                composable("JumpFirst") {
+                    FirstScreen(navController = navController)
+                }
+                composable("JumpSecond") {
+                    SecondScreen(navController = navController)
+                }
             }
         }
     }
